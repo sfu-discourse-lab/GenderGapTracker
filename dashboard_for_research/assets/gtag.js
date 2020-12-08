5e904497269bed1f1e5501199028a8c42c77a60b
prrao87 <prrao87@gmail.com>
2020-12-08 10:23:57 -0500

    Fix gtag

--- dashboard_for_research/assets/gtag.js
+++ dashboard_for_research/assets/gtag.js
@@ -3,6 +3,6 @@
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
     })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
     
-    ga('create', 'UA-172154214-1', 'auto');
+    ga('create', 'UA-XXXXXXXX-X', 'auto');
     ga('send', 'pageview');
     
